f81c2adb55bc2137a11a737135395711caa9415e
Michel Goñi <michelgoni2@gmail.com>
2017-04-22 13:44:00 +0200

    Initial Commit

---
+++ (B)ViperWeatherTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  (B)ViperWeatherTest
+//
+//  Created by Michel Goñi on 22/4/17.
+//  Copyright © 2017 Michel Goñi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
