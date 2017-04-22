1abf38e1d8c7a93d1fd7fc59213aa072dd2a2b11
Michel Goñi <michelgoni2@gmail.com>
2017-04-22 14:08:10 +0200

    First commit adding builder

---
+++ (B)ViperWeatherTest/WeatherListBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  BuildWeatherList.swift
+//  (B)ViperWeatherTest
+//
+//  Created by Michel Goñi on 22/4/17.
+//  Copyright © 2017 Michel Goñi. All rights reserved.
+//
+
+import UIKit
+
+protocol BuildWeatherList {
+    func buildWeatherListModule() -> UIViewController?
+}
---
+++ (B)ViperWeatherTest/WeatherListDefaultBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherListDefaultBuilder.swift
+//  (B)ViperWeatherTest
+//
+//  Created by Michel Goñi on 22/4/17.
+//  Copyright © 2017 Michel Goñi. All rights reserved.
+//
+
+import Foundation
