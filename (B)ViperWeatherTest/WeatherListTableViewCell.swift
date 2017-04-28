86962398de5964054843e21b6cd18145661dbd2a
Michel Goñi <michelgoni2@gmail.com>
2017-04-28 17:33:17 +0200

    Added a custom cell

---
+++ (B)ViperWeatherTest/WeatherListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherListTableViewCell.swift
+//  (B)ViperWeatherTest
+//
+//  Created by Michel Goñi on 28/4/17.
+//  Copyright © 2017 Michel Goñi. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
