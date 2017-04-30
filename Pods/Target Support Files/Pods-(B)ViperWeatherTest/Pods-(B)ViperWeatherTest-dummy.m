fce3605e2fa0f4952619aed373de24b4d4a6b3a3
Michel Goñi <michelgoni2@gmail.com>
2017-04-30 19:52:44 +0200

    Added podfile and Alamofire pod

---
+++ Pods/Target Support Files/Pods-(B)ViperWeatherTest/Pods-(B)ViperWeatherTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods__B_ViperWeatherTest : NSObject
+@end
+@implementation PodsDummy_Pods__B_ViperWeatherTest
+@end
---
+++ Pods/Target Support Files/Pods-(B)ViperWeatherTest/Pods-(B)ViperWeatherTest-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods__B_ViperWeatherTestVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods__B_ViperWeatherTestVersionString[];
+
