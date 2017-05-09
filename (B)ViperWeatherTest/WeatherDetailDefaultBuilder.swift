a1e06c8b0a5ab1ca0ab4b0c9f353e225d311bc9c
Michel Goñi <michelgoni2@gmail.com>
2017-05-09 20:22:36 +0200

    Finishing builder in weatherdetail

--- (B)ViperWeatherTest/WeatherDetailDefaultBuilder.swift
+++ (B)ViperWeatherTest/WeatherDetailDefaultBuilder.swift
@@ -19,12 +19,39 @@ class WeatherDetailDefaultBuilder: WeatherDetailBuilder {
     func buildWeatherDetailModule(withWeatherModel weatherModel: Weather) -> UIViewController? {
         
         buildView()
+        buildInteractor(withWeatherModel: weatherModel)
+        buildPresenter()
+        buildCircularDependencies()
         
         return view as? UIViewController
     }
     
+    
+    // MARK: - Private
     func buildView() {
         
         self.view = WeatherDetailViewController()
+    }
+    
+    func buildInteractor(withWeatherModel weatherModel: Weather) {
+        
+        self.interactorManager = WeatherDetailDefaultInteractorManager(weatherModel: weatherModel)
+    }
+    
+    func buildPresenter() {
+        
+        guard let interactorManager = self.interactorManager, let view = self.view else {
+            assert(false, "No dependencies available")
+            return
+        }
+        self.presenter = WeatherDetailDefaultPresenter(interactorManager: interactorManager, view: view)
+    }
+    
+    fileprivate func buildCircularDependencies() {
+        guard let presenter = self.presenter, let view = self.view as? WeatherDetailViewController else {
+            assert(false, "No dependencies available")
+            return
+        }
+        view.presenter = presenter
     }
 }
